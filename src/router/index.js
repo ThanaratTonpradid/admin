f354ec94f1c3cd534e9ce006572e3ecb74e10ea2
Tonpradit.T <thanarat@dollarsign.co>
2022-09-01 15:47:09 +0700

    refactor: format code

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,21 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      component: () => import("@/layouts/PublicLayout.vue"),
+      path: '/',
+      component: () => import('@/layouts/PublicLayout.vue'),
       children: [
         {
-          path: "/",
-          name: "home",
-          component: () => import("@/pages/HomePage.vue"),
+          path: '/',
+          name: 'home',
+          component: () => import('@/pages/HomePage.vue'),
         },
         {
-          path: "/about",
-          name: "about",
-          component: () => import("@/pages/AboutPage.vue"),
+          path: '/about',
+          name: 'about',
+          component: () => import('@/pages/AboutPage.vue'),
         },
       ],
     },
